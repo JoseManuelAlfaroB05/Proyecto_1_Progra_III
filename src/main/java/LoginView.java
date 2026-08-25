daee6213215d918b7fb25ffc0084485fe62d1163
josem <jose.alfaro.bogantes@est.una.ac.cr>
2026-08-25 15:36:29 -0600

    Agregar Mini interface login

--- src/main/java/LoginView.java
+++ src/main/java/LoginView.java
@@ -1,2 +1,10 @@
+import javax.swing.*;
+
 public class LoginView {
+    private JPanel mainPanel;
+    private JTextField UserTextField;
+    private JLabel PaswordTextField;
+    private JPasswordField passwordField1;
+    private JButton forgotPasswordButton;
+
 }
