661f2aca91b437e374540bd82944cf9201fd5413
josem <jose.alfaro.bogantes@est.una.ac.cr>
2026-08-31 22:54:53 -0600

    Implementar Interface de Reserva 1

--- src/main/java/Views/MainView.java
+++ src/main/java/Views/MainView.java
@@ -1,23 +1,40 @@
 package Views;
 
 import Models.User;
+import com.github.lgooddatepicker.components.DatePicker;
+import com.github.lgooddatepicker.components.TimePicker;
 
 import javax.swing.*;
 
 public class MainView extends JFrame{
-    private JPanel panel1;
+    private JPanel principalPanel;
+    private JPanel contentPanel;
     private JTabbedPane tabbedPane1;
-    private JTextField textField1;
-    private JButton button1;
-    private JTextField actividadtextField;
-    private JButton reservarButton;
+    private JTextField textFieldReservaAutomatica;
+    private JButton buttonReservaAutomatica;
+    private JTextField textFieldActividad;
     private JButton button2;
+    private JButton button3;
+    private JLabel LabelActividad;
+    private JPanel JPanelLower;
+    private JPanel PanelFecha;
+    private JLabel PanelInicio;
+    private JPanel PanelFinal;
+    private TimePicker HoraFin;
+    private TimePicker HoraInicio;
+    private DatePicker datePicker;
+    private JRadioButton radioButtonLab;
+    private JRadioButton radioButton1;
+    private JTextField textField1;
+    private JRadioButton radioButton2;
+    private JTextField textField2;
+    private JButton historialDeReservasButton;
 
 
     public MainView(User usuarioLogueado) {
-        setContentPane(panel1);
+        setContentPane(principalPanel);
         setTitle("Sistema de Reserva de Recursos");
-        setSize(600, 400);
+        setSize(720, 480);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
