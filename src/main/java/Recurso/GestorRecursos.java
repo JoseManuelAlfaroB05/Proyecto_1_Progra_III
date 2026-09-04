760d69b018328bfdf536844a67e07bfb1f394dab
josem <jose.alfaro.bogantes@est.una.ac.cr>
2026-09-04 10:16:34 -0600

    Implementacion Gestror de recursos

--- src/main/java/Recurso/GestorRecursos.java
+++ src/main/java/Recurso/GestorRecursos.java
@@ -1,5 +1,20 @@
 package Recurso;
 
+import Persistencia.RecursoXMLDao;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestorRecursos {
 
+    private ArrayList<Recurso> recursos;
+
+    public GestorRecursos() {
+        RecursoXMLDao dao = new RecursoXMLDao();
+        recursos = new ArrayList<>(dao.listarTodos());
+    }
+
+    public ArrayList<Recurso> getRecursos() {
+        return recursos;
+    }
 }
