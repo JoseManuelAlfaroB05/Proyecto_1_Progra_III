fdd411165507401c1a098a5d0ab16ee34e32f95b
josem <jose.alfaro.bogantes@est.una.ac.cr>
2026-09-09 15:05:21 -0600

    meter funcionarios a mainpanel

--- src/main/java/Views/MainView.java
+++ src/main/java/Views/MainView.java
@@ -1,26 +1,23 @@
 package Views;
 
 import Models.User;
-import Views.Reservas.ReservaView;
-import Views.Funcionarios.FuncionariosView;
-import Models.Rol;
-
 import Views.Calendarizacion.CalendarizacionView;
-import Views.Categoria.CategoriaView;
-
+import Views.Funcionarios.FuncionariosView;
+import Views.Reservas.ReservaView;
 
 import javax.swing.*;
-import java.awt.BorderLayout;
+import java.awt.*;
 
 public class MainView extends JFrame {
 
     private JPanel principalPanel;
     private JPanel contetPanel;
     private JTabbedPane tabbedPane;
+
     private JPanel reservaPanel;
     private JPanel calendarizacion;
-    private JPanel TabbedPanelUsuariosPanel;
     private JPanel CategoriaView;
+    private JPanel funcionariosPanel;
 
     private User usuarioLogueado;
 
@@ -28,32 +25,48 @@ public class MainView extends JFrame {
 
         this.usuarioLogueado = usuarioLogueado;
 
-        setContentPane(principalPanel);
         setTitle("Sistema de Reserva de Recursos - Usuario logueado: "
                 + usuarioLogueado.getVarId());
-        setSize(1000, 600);
+
+        setContentPane(principalPanel);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        ReservaView reservaView = new ReservaView(usuarioLogueado);
+        setSize(1000, 700);
+        setLocationRelativeTo(null);
+
+        cargarVistas();
+    }
+
+    private void cargarVistas() {
+
+        ReservaView reservaView =
+                new ReservaView(usuarioLogueado);
 
         reservaPanel.setLayout(new BorderLayout());
-        reservaPanel.add(reservaView, BorderLayout.CENTER);
+        reservaPanel.add(
+                reservaView,
+                BorderLayout.CENTER
+        );
 
 
-        // No eliminar las pestañas definidas en el diseñador, para que "Categoria" y "Calendarizacion" sigan visibles.
-        if (usuarioLogueado.getVarRol() == Rol.ADMINISTRADOR) {
-            tabbedPane.insertTab("Funcionarios", null, new FuncionariosView(), null, 0);
-        }
-
-        CalendarizacionView calendarizacionView = new CalendarizacionView();
+        CalendarizacionView calendarizacionView =
+                new CalendarizacionView();
 
         calendarizacion.setLayout(new BorderLayout());
-        calendarizacion.add(calendarizacionView, BorderLayout.CENTER);
+        calendarizacion.add(
+                calendarizacionView,
+                BorderLayout.CENTER
+        );
 
-        CategoriaView categoria = new CategoriaView();
 
-        CategoriaView.setLayout(new BorderLayout());
-        CategoriaView.add(categoria, BorderLayout.CENTER);
+        FuncionariosView funcionariosView =
+                new FuncionariosView();
 
+        funcionariosPanel.setLayout(new BorderLayout());
+        funcionariosPanel.add(
+                funcionariosView,
+                BorderLayout.CENTER
+        );
     }
 }
